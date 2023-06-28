b0a8a2af7c8cff07adaf020b2361a522592d30f1
Jonathan <contact@jonathan-ribiere.fr>
2023-06-28 22:16:13 +0200

    mise en place du navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './navbar.css'
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+      <div className='navbar-logo'>Logo</div>
+      <div className='navbar-list'>List</div>
+    </div>
+  )
+}
