cc256f89f9c32091f240960f81effce6ed74e514
Jonathan <contact@jonathan-ribiere.fr>
2023-06-29 03:37:50 +0200

    debut de la mise en forme de la page fiche logement

--- src/pages/FicheLogement/ficheLogement.jsx
+++ src/pages/FicheLogement/ficheLogement.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from "react";
+import Header from "../../components/Header";
 
-export default function ficheLogement() {
-  return (
-    <div>ficheLogement</div>
-  )
+export default function ficheLogement(props) {
+ 
+    return (
+    <div>
+      <Header />
+      <div>
+        <div>image</div>
+        <div>
+          <div>
+            <div>
+              <h1>titre</h1>
+              <p>region</p>
+            </div>
+            <div>tag</div>
+          </div>
+          <div>
+            <div>
+              <div>nom</div>
+              <div>photo</div>
+            </div>
+            <div>etoile</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
