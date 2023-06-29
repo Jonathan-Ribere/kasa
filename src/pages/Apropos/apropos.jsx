16c6c8a7cfa511b5e6f016f57ba5ef4796580979
Jonathan <contact@jonathan-ribiere.fr>
2023-06-30 01:46:43 +0200

    mise en place du dropdown

--- src/pages/Apropos/apropos.jsx
+++ src/pages/Apropos/apropos.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/Header/header";
 import cover from "../../images/Apropos.png";
 import "./apropos.scss";
 
 export default function Aporpos() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
       <Header />
@@ -11,6 +17,25 @@ export default function Aporpos() {
         <div className="cover">
           <img src={cover} className="cover-img" alt="Image de site" />
         </div>
+        <section>
+        <div className="dropdown">
+      <div className="dropdown-toggle" onClick={toggleDropdown}>
+        Dropdown
+      </div>
+      {isOpen && (
+        <ul className="dropdown-menu">
+          <li>ici le text</li>
+        
+        </ul>
+      )}
+    </div>
+  
+
+
+
+
+
+        </section>
       </main>
     </>
   );
