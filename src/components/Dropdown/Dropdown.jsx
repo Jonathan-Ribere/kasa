12871668078a73519951c1bb0d600ac13a394182
Jonathan <contact@jonathan-ribiere.fr>
2023-06-30 02:46:44 +0200

    ajout des arrow haut et bas sur les dropdown

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "./dropdown.scss";
+import haut from "../../images/icones/arrow_haut.png";
+import bas from "../../images/icones/arrow_bas.png";
 
 export default function Dropdown(props) {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,7 +12,8 @@ export default function Dropdown(props) {
   return (
     <div className="dropdown">
       <div className="dropdown-toggle" onClick={toggleDropdown}>
-       {props.titre}
+        <h2>{props.titre}</h2>
+        <img src={isOpen ? haut : bas} alt="Flèche" className="arrow" />
       </div>
       {isOpen && (
         <ul className="dropdown-menu">
