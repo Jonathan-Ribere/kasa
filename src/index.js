de7e2d20804f0dd7e2d4f0166604da14901b4ee9
Jonathan <contact@jonathan-ribiere.fr>
2023-07-01 04:05:38 +0200

    creation du carrouselle et corection des erreur

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './global.scss'
-import Home from './pages/Home/home'
-import Apropos from './pages/Apropos/apropos'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './global.scss';
+import Home from './pages/Home/home';
+import Apropos from './pages/Apropos/apropos';
 import FicheLogement from './pages/FicheLogement/FicheLogement';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
@@ -16,7 +16,5 @@ root.render(
         <Route path="/fiche-logement/:id" element={<FicheLogement />} />
       </Routes>
     </Router>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
-
-
--- src/pages/FicheLogement/ficheLogement.jsx
+++ src/pages/FicheLogement/ficheLogement.jsx
@@ -1,27 +1,55 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import Header from "../../components/Header/header";
-import "./ficheLogement.scss";
-import data from "../../db";
-import etoileRed from "../../images/icones/etoileRed.png";
-import etoileGris from "../../images/icones/etoileGris.png";
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Header from '../../components/Header/header';
+import './ficheLogement.scss';
+import data from '../../db';
+
+import flecheD from '../../images/icones/flecheDroite.png';
+import flecheG from '../../images/icones/flecheGauche.png';
+
+import etoileRed from '../../images/icones/etoileRed.png';
+import etoileGris from '../../images/icones/etoileGris.png';
 
 export default function FicheLogement(props) {
-  // Récupérer l'ID à partir de l'URL
   const { id } = useParams();
-
-  // Rechercher l'objet correspondant à l'ID dans le tableau de données
   const logement = data.find((item) => item.id === id);
 
   if (!logement) {
     return <div>Ce logement n'existe pas.</div>;
   }
 
+  const [currentIndex, setCurrentIndex] = useState(0);
+  
+  const handlePrevious = () => {
+    setCurrentIndex((prevIndex) =>
+    prevIndex > 0 ? prevIndex - 1 : logement.pictures.length - 1
+    );
+  };
+  
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % logement.pictures.length);
+  };
+  
   return (
     <div>
       <Header />
       <div className="main">
-        <div className="img">image</div>
+        <div className="carousel">
+          {logement.pictures.length > 1 && (
+            <button onClick={handlePrevious}>
+              <img src={flecheG} alt="Flèche gauche" />
+            </button>
+          )}
+
+          <img src={logement.pictures[currentIndex]} alt="image" />
+
+          {logement.pictures.length > 1 && (
+            <button onClick={handleNext}>
+              <img src={flecheD} alt="Flèche droite" />
+            </button>
+          )}
+        </div>
+
         <div className="info">
           <div className="info-global">
             <h1 className="info-global_h1">{logement.title}</h1>
