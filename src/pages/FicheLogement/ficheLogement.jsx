5071b8f42b486b843e4c25034e3efb427ba71ccb
Jonathan <contact@jonathan-ribiere.fr>
2023-07-01 03:20:37 +0200

    ajout de la partie etoile nom et photo

--- src/pages/FicheLogement/ficheLogement.jsx
+++ src/pages/FicheLogement/ficheLogement.jsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 import Header from "../../components/Header/header";
 import "./ficheLogement.scss";
 import data from "../../db";
+import etoileRed from "../../images/icones/etoileRed.png";
+import etoileGris from "../../images/icones/etoileGris.png";
 
 export default function FicheLogement(props) {
   // Récupérer l'ID à partir de l'URL
@@ -33,10 +35,22 @@ export default function FicheLogement(props) {
             </span>
           </div>
           <div className="user">
-            <div className="user-etoile">etoiles</div>
+            <div className="user-etoile">
+              {Array.from({ length: 5 }, (_, index) => (
+                <img
+                  key={index}
+                  src={index < logement.rating ? etoileRed : etoileGris}
+                  alt="étoile"
+                />
+              ))}
+            </div>
             <div className="user-info">
               <div className="user-info_name">{logement.host.name}</div>
-              <div className="user-info_pic">{logement.host.picture}</div>
+              <img
+                src={logement.host.picture}
+                alt="Profile Picture"
+                className="user-info_pic"
+              />
             </div>
           </div>
         </div>
