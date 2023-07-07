c09d16c3d4850f18bc3de24355adf2fdc5c7687d
Jonathan <contact@jonathan-ribiere.fr>
2023-07-07 20:33:08 +0200

    ajout des fleche en plus grand format pour la version desktop et ajout de useEffect pour pouvoir suivre l'etat de l'ecrant pour choisir quel version de l'icone afficher , finition du scss pour le responsive en version mobil et ordi

--- src/pages/FicheLogement/ficheLogement.jsx
+++ src/pages/FicheLogement/ficheLogement.jsx
@@ -1,96 +1,81 @@
-import React, { useState } from "react";
-import { Navigate, useParams } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { Navigate, useParams } from "react-router-dom";
 import Header from "../../components/Header/header";
+import Footer from "../../components/Footer/Footer";
 import "./ficheLogement.scss";
 import data from "../../db";
-
 import Error from "../Error/Error";
-
 import flecheD from "../../images/icones/flecheDroite.png";
 import flecheG from "../../images/icones/flecheGauche.png";
-
+import flecheD1 from "../../images/icones/gaucheGrandFormat.png";
+import flecheD2 from "../../images/icones/droitGrandFormat.png";
 import etoileRed from "../../images/icones/etoileRed.png";
 import etoileGris from "../../images/icones/etoileGris.png";
 import Dropdown from "../../components/Dropdown/Dropdown";
 
 export default function FicheLogement() {
-  {
-    /** hook useParams() de React Router pour extraire la valeur de l'URL dynamique
- nommée "id". Cette valeur sera stockée dans la variable id. */
-  }
   const { id } = useParams();
-
-  {
-    /** Cette méthode find() utilise le tableau data pour rechercher l'élément du logement
-   correspondant à l'ID extrait de l'URL. L'ID du logement est comparé avec chaque élément
-   du tableau jusqu'à ce qu'une correspondance soit trouvée. L'élément correspondant est
-   stocké dans la variable logement.  */
-  }
   const logement = data.find((item) => item.id === id);
 
   if (!logement) {
-      return (
-        <Navigate to="/error" replace={true} />
-      )
+    return <Navigate to="/error" replace={true} />;
   }
 
-  {
-    /** le hook useState() pour créer une variable d'état currentIndex, et une fonction setCurrentIndex
- pour la mettre à jour. La valeur initiale de currentIndex est définie sur 0. */
-  }
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handlePrevious = () => {
-    {
-      /** la fonction setCurrentIndex pour mettre à jour currentIndex en utilisant une fonction de rappel.
-   La valeur de currentIndex est vérifiée : si elle est supérieure à zéro, elle est décrémentée de 1
-   pour passer à l'image précédente. Sinon, elle est définie sur logement.pictures.length - 1
-   pour boucler vers la dernière image de la liste. */
-    }
     setCurrentIndex((prevIndex) =>
       prevIndex > 0 ? prevIndex - 1 : logement.pictures.length - 1
     );
   };
 
-  {
-    /**handleNext met à jour l'index actuel pour afficher l'image suivante dans une liste d'images */
-  }
   const handleNext = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % logement.pictures.length);
   };
+
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 1024);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth < 1024);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div>
       <Header />
       <div className="main">
-      <div className="carousel-container">
-  {logement.pictures.length > 1 && (
-    <span className="arrow left" onClick={handlePrevious}>
-      <img src={flecheG} alt="Flèche gauche" />
-    </span>
-  )}
+        <div className="carousel-container">
+          {logement.pictures.length > 1 && (
+            <span className="arrow left" onClick={handlePrevious}>
+              <img src={isMobile ? flecheG : flecheD1} alt="Flèche gauche" />
+            </span>
+          )}
 
-  <img
-    src={logement.pictures[currentIndex]}
-    alt="image"
-    className="carousel-img"
-  />
+          <img
+            src={logement.pictures[currentIndex]}
+            alt="image"
+            className="carousel-img"
+          />
 
-  {logement.pictures.length > 1 && (
-    <span className="arrow right" onClick={handleNext}>
-      <img src={flecheD} alt="Flèche droite" />
-    </span>
-  )}
+          {logement.pictures.length > 1 && (
+            <span className="arrow right" onClick={handleNext}>
+              <img src={isMobile ? flecheD : flecheD2} alt="Flèche droite" />
+            </span>
+          )}
 
-  {logement.pictures.length > 1 && (
-    <span className="image-counter">
-      {currentIndex + 1}/{logement.pictures.length}
-    </span>
-  )}
-</div>
-
-
-
+          {logement.pictures.length > 1 && (
+            <span className="image-counter">
+              {currentIndex + 1}/{logement.pictures.length}
+            </span>
+          )}
+        </div>
         <div className="info">
           <div className="info-global">
             <h1 className="info-global_h1">{logement.title}</h1>
@@ -138,6 +123,7 @@ export default function FicheLogement() {
           />
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
