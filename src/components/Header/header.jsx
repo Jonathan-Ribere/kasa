c0fd79987cccffba2bf64c8b8a9ff955347adbd0
Jonathan <contact@jonathan-ribiere.fr>
2023-07-10 14:25:33 +0200

    correction des lien active dans le composant Header

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./header.scss";
 import logo from "../../images/logo.svg";
 
 export default function Header() {
+  const location = useLocation();
+
   return (
     <div className="header">
       <div className="header-logo">
@@ -15,13 +17,13 @@ export default function Header() {
       </div>
 
       <nav className="header-lien color_red">
-        <div className="divLien lien1">
+        <div className={`divLien lien1 ${location.pathname === '/' ? 'active' : ''}`}>
           <Link to="/" className="header-lien_text">
             ACCUEIL
           </Link>
         </div>
 
-        <div className="divLien lien2">
+        <div className={`divLien lien2 ${location.pathname === '/apropos' ? 'active' : ''}`}>
           <Link to="/apropos" className="">
             A PROPOS
           </Link>
